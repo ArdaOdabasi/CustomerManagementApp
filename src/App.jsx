4f169f8009341c4426ff2ebbb1a533120e1e5cad
Arda Odabasi <ardaodabasi@hotmail.com>
2023-08-05 02:34:33 +0300

    Form design completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CustomerForm from './components/CustomerForm/CustomerForm';
 function App() {
   return (
     <div className="App">
+      <h1>Customer Manage System</h1>
       <CustomerForm />
       <CustomerList />
     </div>
--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./CustomerForm.css"
 
 const CustomerForm = () => {
   return (
-    <div>CustomerForm</div>
-  )
+    <form className='customer-form'>
+      <input type='text' className='customer-input' placeholder='Add a new customer'></input>
+      <button>
+        <i className='bi bi-plus-lg'></i>
+      </button>
+    </form>
+  );
 }
 
 export default CustomerForm
